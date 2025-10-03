9388884f3d8cfebdd58520ce3014036953f470db
Jamina-ENSI <amina.jarraya@ensi-uma.tn>
2025-10-03 16:51:34 +0200

    dev commitc

--- src/application/Test.java
+++ src/application/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		//Mon message
 		System.out.println("Je suis le master");
 		System.out.println("Je suis le dev");
+		System.out.println("Je modifie le code en tant que developpeur");
 
 	}
 
